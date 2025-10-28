8f03db3a7a02a836900865838b104245b6929c2b
dromanov <lukaviy2la@gmail.com>
2025-10-28 15:37:08 +0300

    project_run/instal drf3/16/0

--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #others
+    'rest_framework',
     #local
     'app_run',
 ]
