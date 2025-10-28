603c6feae2459fdc92d3ab05e1646dbcb74bcf79
dromanov <lukaviy2la@gmail.com>
2025-10-28 15:58:51 +0300

    project_run/task 0

--- app_run/views.py
+++ app_run/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def contacts_view(request):
+    return Response(
+        {
+            'company_name': 'Рога и панцирь',
+            'slogan': 'Бегаем или за кем-то или от кого-то',
+            'contacts': 'NDA'
+        }
+    )
--- project_run/urls.py
+++ project_run/urls.py
@@ -19,6 +19,10 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
+from app_run import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/company_details/', views.contacts_view)
 ]
