2aa1aceb4291218041e8ad0ba06f2567210227bc
dromanov <lukaviy2la@gmail.com>
2025-10-31 09:45:50 +0300

    project_run/task 4-update

--- app_run/views.py
+++ app_run/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework import viewsets
 
@@ -25,6 +26,8 @@ class RunViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['=first_name', '=last_name,']
 
     def get_queryset(self):
         qs = self.queryset.exclude(is_superuser=True)
