ae83092cda1b3aa7b4eaaee6d6db8099e8f39248
dromanov <lukaviy2la@gmail.com>
2025-10-31 10:37:03 +0300

    project_run/task 5-add paths

--- app_run/models.py
+++ app_run/models.py
@@ -3,6 +3,12 @@ from django.conf import settings
 
 # Create your models here.
 class Run(models.Model):
+    STATUS_CHOICES = [
+        ("init", "init"),
+        ("in_progres", "in_progres"),
+        ("finished", "finished"),
+    ]
     created_at = models.DateTimeField(auto_now_add=True)
     comment = models.TextField()
     athlete = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='runs')
+    status = models.CharField(max_length=16, choices=STATUS_CHOICES, default='init')
--- app_run/views.py
+++ app_run/views.py
@@ -1,9 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import viewsets
+from rest_framework import status
 
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 from .models import Run
 from .serializers import RunSerializer, UserSerializer
@@ -17,6 +21,59 @@ def contacts_view(request):
             'contacts': 'NDA'
         }
     )
+
+
+class StartRunApiView(APIView):
+    def post(self, request, run_id):
+        current = get_object_or_404(Run, id=run_id)
+
+        if current.status != Run.STATUS_CHOICES[0][0]:
+            return Response(
+                {
+                    "detail": "Run already started or finished!",
+                    "id": current.id,
+                    "status": current.status,
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        current.status = Run.STATUS_CHOICES[1][0]
+        current.save(update_fileds=["status"])
+
+        return Response(
+            {
+                "id": current.id,
+                "status": current.status,
+            },
+            status=status.HTTP_200_OK,
+        )
+
+
+class StopRunApiView(APIView):
+    def post(self, request, run_id):
+        current = get_object_or_404(Run, id=run_id)
+
+        if current.status != Run.STATUS_CHOICES[1][0]:
+            return Response(
+                {
+                    "detail": "Run already started or finished!",
+                    "id": current.id,
+                    "status": current.status,
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        current.status = Run.STATUS_CHOICES[2][0]
+        current.save(update_fileds=["status"])
+
+        return Response(
+            {
+                "id": current.id,
+                "status": current.status,
+            },
+            status=status.HTTP_200_OK,
+        )
+
 
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.select_related('athlete').all()
